d052505264ffed0b84c506b26787b99d40b96a19
Ben-Wheeler333 <chemlovexx@icloud.com>
2022-04-21 15:58:15 -0500

    page-template

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-console.log(profileDataArgs);
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
+const profileDataArgs = process.argv.slice(2);
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-   
-  };
-  
-  printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw(err);
+
+  console.log('Portfolio complete! Checkout index.html to see the output!');
+});
